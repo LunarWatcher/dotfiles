0113203ef486fe3e5f306b7fe432610cf1cb6ff4
OliviaZoe0 <zoe.i2k1@gmail.com>
2020-01-28 21:15:41 +0100

    Reformat argument types, add backtick autokey mapping

--- init.sh
+++ init.sh
@@ -2,6 +2,24 @@ NC='[0m'
 GREEN='[1;32m'
 RED='[1;31m'
 PINK='[1;95m'
+
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    POSITIONAL+=("$1") 
+    shift # past argument
+done
+
+function has() {
+
+    case "${POSITIONAL[@]}" in *"$1"*)
+        return 1;
+    esac;
+}
+
 echo -e "${PINK}Welcome to Olivia's system initializer."
 echo "This bash script initializes a system with my standards and dotfiles."
 echo "The purpose of this script is simply to let me not have to bother with manual installs of stuff whenever I start a new system."
@@ -9,21 +27,55 @@ echo "The end of this script also copies the dotfiles over, so if you'd just lik
 echo "Reading the script first is highly encouraged, because there's not a lot of prompts. Check what stuff does before you agree to it."
 echo -e "${NC}"
 
-echo -e "${RED}All questions are y/n${NC}"
-read -p "${GREEN}Install packages? (Assumes apt is present)${NC} " -n 1;
-echo "";
+has "--help"
+hr=$?
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo apt-add-repository universe
-    sudo apt update
-    sudo apt install -y thefuck curl python3-pip python-pkg-resources
+if [[ ${#POSITIONAL[@]} == 0 ]]; then
+    
+    hr=1;
 fi;
 
-read -p "${GREEN}Build Vim? (Assumes apt, git, and a properly linked compiler are present)${NC} " -n 1;
-echo "";
+if [[ $hr == 1 ]];
+then
+    echo "Usage: ./init.sh flags"
+    echo "At least one flag is required to take any action. 0 flags prints this message. Several flags can be used together. "
+    echo "  --packages   - installs standard packages"
+    echo "  --vim        - Builds Vim from source"
+    echo "  --polybar    - Builds polybar from source"
+    echo "  --dotfiles   - Installs dotfiles"
+    echo "  --autokey    - Installs autokey + config (Note: config set up for a Scandinavian keyboard)"
+    echo "  --vim-plug   - Bootstraps vim-plug and runs PlugInstall afterwards. This option assumes you have Vim already."
+    echo "  --nerdfonts  - Installs nerdfonts. WARNING: This has a download size of 5.37 GB (27.01.20) and will take a while to complete, but is also run last, so it doesn't block the other actions."
+    exit;
+fi;
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then 
-    echo -e "${GREEN}Installing vim build deps...${NC}"
+has "--packages"
+packages=$?
+has "--vim"
+vim=$?
+has "--polybar"
+polybar=$?
+has "--dotfiles"
+dotfiles=$?
+has "--autokey"
+autokey=$?
+has "--vim-plug"
+vimplug=$?
+has "--nerdfonts"
+nerdfonts=$?
+
+### Install standard packages ###
+if [[ $packages == 1 ]]; then
+    echo -e "${GREEN}Installing packages...${NC}"
+    sudo apt-add-repository universe
+    sudo apt update
+    sudo apt install -y thefuck curl python3-pip python-pkg-resources cmake build-essential
+    echo -e "${GREEN}Done.${NC}";
+fi;
+
+### Build and install Vim ###
+if [[ $vim == 1 ]]; then 
+    echo -e "${GREEN}Installing vim build deps...${NC}";
     sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
@@ -31,7 +83,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     git clone https://github.com/vim/vim.git
     cd vim
     cd src
-
+    echo -e "${GREEN}Done. Building vim...${NC}";
     # gVim
     ./configure --enable-gui=gtk2 --with-features=huge --enable-multibyte \
         --enable-rubyinterp=yes --enable-python3interp=yes \
@@ -46,11 +98,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo -e "${GREEN}Vim installed.${NC}"
 fi;
 
-read -p "${GREEN}Build polybar?$NC " -n 1;
-echo ""
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo apt install -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+### Install polybar ###
+if [[ $polybar == 1 ]]; then
+    echo -e "${GREEN}Building polybar...${NC}"
+    sudo apt install -y cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
     git clone --recursive https://github.com/polybar/polybar
     cd polybar 
 
@@ -61,40 +112,48 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
     cd ../..
     rm -rf polybar
+    
+    echo -e "${GREEN}Done.${NC}"
 fi;
 
-echo -e "${RED}WARNING:${NC} Proceeding with the dotfiles copy will overwrite your local dotfiles, if present. Copy any dotfiles you'd like to keep before running this. Skipping is also an option if this isn't what you signed up for. "
-read -p "Continue? " -n 1;
-echo "";
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then 
+### Copy dotfiles ###
+if [[ $dotfiles == 1 ]]; then 
     echo -e "${GREEN}Copying Linux dotfiles...${NC}";
     echo "vimrc...";
     cp .vimrc ~/
     echo "bashrc...";
     cp .bashrc ~/
-    cp -r config/* ~/.config/
+    if [[ $autokey == 1 ]]; then
+        sudo apt install -y autokey-gtk
+        rsync -av --progress config ~/.config/
+    else
+        rsync -av --progress config/ ~/.config/ --exclude "autokey" 
+    fi;
     echo -e "${GREEN}Done. Enjoy your install!${NC} 💜";
 fi;
 
-read -p "${GREEN}Bootstrap vim-plug?${NC} " -n 1;
-echo "";
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+### Bootstrap vim-plug ###
+if [[ $vimplug == 1 ]]; then
+    echo -e "${GREEN}Installing vim-plug...${NC}";
+    
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo "vim-plug bootstrapped.";
+    echo "vim-plug bootstrapped. Running PlugInstall now for your convenience...";
     vim +PlugInstall
+
+    
+    echo -e "${GREEN}Done.${NC}";
+
 fi;
 
-read -p "${GREEN}Install ryanoasis/nerd-fonts? Note: Installs _all_ fonts. Note that the download is slow due to the huge amount of data required. Don't run this if you're low on data, or don't want to wait around. This is the last step of the bootstrapping process; there's nothing else after this. (27.01.20: 5.38GB)${NC}" -n 1;
-echo ""
+### Install nerd-fonts ###
+if [[ $nerdfonts == 1 ]]; then
+    echo -e "${GREEN}Installing ryanoasis/nerd-fonts. This will take a while, depending on your internet speed.${NC}";
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then 
     git clone https://github.com/ryanoasis/nerd-fonts
 
     cd nerd-fonts && ./install.sh
-
+    echo -e "${GREEN}Done. The nerd-fonts folder has also been preserved due to its download size. If you don't need it, you can remove it manually.${NC}";
 fi;
 
 echo -e "${PINK}Bootstrapper run. Enjoy your system!${NC}";
