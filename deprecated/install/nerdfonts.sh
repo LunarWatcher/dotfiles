f380333a7589059816cf79d3eb009a7af6abf276
Olivia <zoe.i2k1@gmail.com>
2022-04-27 01:34:15 +0200

    nuke deprecated scripts

--- deprecated/install/nerdfonts.sh
+++
@@ -1,7 +0,0 @@
-
-echo -e "${GREEN}Installing ryanoasis/nerd-fonts. This will take a while, depending on your internet speed.${NC}";
-
-git clone https://github.com/ryanoasis/nerd-fonts
-
-cd nerd-fonts && ./install.sh
-echo -e "${GREEN}Done. The nerd-fonts folder has also been preserved due to its download size. If you don't need it, you can remove it manually.${NC}";
