2218fcef4e3060db6abcb22fe560c6d794f975c4
Olivia <zoe.i2k1@gmail.com>
2023-10-30 11:45:59 +0100

    Convert fixBluetooth.sh to an alias

--- scripts/fixBluetooth.sh
+++
@@ -1,5 +0,0 @@
-# Patch for fixing bluetooth that doesn't want to connect.
-# This isn't a guaranteed patch, but it works for my main problem 
-pactl unload-module module-bluetooth-discover 
-pactl load-module module-bluetooth-discover 
-
