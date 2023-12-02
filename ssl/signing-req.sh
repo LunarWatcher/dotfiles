d34b5b280032e1dbddc14d824f461b9e632ee266
Olivia (Zoe) <zoe.i2k1@gmail.com>
2023-12-02 19:19:02 +0100

    Typo

--- ssl/signing-req.sh
+++ ssl/signing-req.sh
@@ -1,2 +1,2 @@
-sudo openssl req -new -nodes -out nova.crt -keyout nova.key -config config.conf
+sudo openssl req -new -nodes -out nova.csr -keyout nova.key -config config.conf
 sudo openssl x509 -req -in nova.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out nova.cert -days 3650 -extensions v3_req -extfile config.conf
