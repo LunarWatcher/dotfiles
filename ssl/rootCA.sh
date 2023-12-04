04a35df0cc55527de5ff9891ff6cc39bccf23ef1
Olivia <zoe.i2k1@gmail.com>
2023-12-04 18:35:29 +0100

    Standardise SSL shit for reuse elsewhere

--- ssl/rootCA.sh
+++ ssl/rootCA.sh
@@ -1,2 +1,4 @@
-sudo openssl genrsa -out rootCA.key 4096
-sudo openssl req -x509 -new -nodes -key rootCA.key -sha512 -days 3650 -out rootCA.crt
+CA=${CANAME:-rootCA}
+
+openssl genrsa -out $CA.key 4096
+openssl req -x509 -new -nodes -key $CA.key -sha512 -days 3650 -out $CA.crt
--- ssl/signing-req.sh
+++ ssl/signing-req.sh
@@ -1,2 +1,12 @@
-sudo openssl req -new -nodes -out nova.csr -keyout nova.key -config config.conf
-sudo openssl x509 -req -in nova.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out nova.cert -days 3650 -extensions v3_req -extfile config.conf
+#!/bin/bash
+
+if [[ "$KEYNAME" == "" ]]; then
+    echo "You must specify the name of the key using KEYNAME=example ./signing-req.sh"
+    exit -1
+fi
+
+CA=${CANAME:-rootCA}
+echo "Using CA file $CA"
+
+openssl req -new -nodes -out $KEYNAME.csr -keyout $KEYNAME.key -config config.conf
+openssl x509 -req -in $KEYNAME.csr -CA $CA.crt -CAkey $CA.key -CAcreateserial -out $KEYNAME.cert -days 3650 -extensions v3_req -extfile config.conf
