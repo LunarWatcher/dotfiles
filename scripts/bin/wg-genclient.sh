e23f5e9d27aa6c896b0ea75738b2c6964bf4fbe0
Olivia <zoe.i2k1@gmail.com>
2024-01-19 19:42:22 +0100

    sudo bash -c is not required in this context; the entire script has to be run as sudo, so it's all one continuous sudo context

--- scripts/bin/wg-genclient.sh
+++ scripts/bin/wg-genclient.sh
@@ -11,13 +11,13 @@ if [[ hostname == "" ]]; then
     echo "You need to supply a hostname as the first parameter"
     exit -69
 fi
-
+cd /etc/wireguard
 umask 077
-sudo bash -c "cd /etc/wireguard && wg genkey | tee '${hostname}.key' | wg pubkey > '${hostname}'.pub"
-sudo bash -c "cd /etc/wireguard && wg genpsk > '${hostname}.psk'"
 
+wg genkey | tee '${hostname}.key' | wg pubkey > '${hostname}'.pub
+wg genpsk > '${hostname}.psk'
 
-sudo cat <<EOF | sudo tee -a /etc/wireguard/wg0.conf
+cat <<EOF | tee -a wg0.conf
 
 [Peer]
 PublicKey = $(cat "${hostname}.pub")
@@ -27,18 +27,18 @@ EOF
 
 
 # reload wireguard
-sudo wg syncconf wg0 <(wg-quick strip wg0)
+wg syncconf wg0 <(wg-quick strip wg0)
 
 echo "Client config:"
-cat <<EOF | sudo tee /etc/wireguard/${hostname}.conf
+cat <<EOF | tee ${hostname}.conf
 [Interface]
 Address = 10.100.0.2/32, fd08:4711::2/128
 DNS = 192.168.0.179
-PrivateKey = $(sudo cat /etc/wireguard/${hostname}.key)
+PrivateKey = $(cat ${hostname}.key)
 
 [Peer]
 AllowedIPs = 10.100.0.1/32, fd08:4711::1/128
 Endpoint = ${HOMELAB_DOMAIN}:47111
 EOF
-sudo cat /etc/wireguard/${hostname}.conf
-sudo qrencode -t ansiutf8 < /etc/wireguard/${hostname}.conf
+cat ${hostname}.conf
+qrencode -t ansiutf8 < /etc/wireguard/${hostname}.conf
