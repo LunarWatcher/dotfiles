630f536c4a85e4ef71b231d3121d5dabf83ea279
Olivia <zoe.i2k1@gmail.com>
2024-02-13 02:01:16 +0100

    Typo
    I should've seen that

--- automation/homelab/baikal.sh
+++ automation/homelab/baikal.sh
@@ -6,8 +6,8 @@
 
 if [ -d /opt/baikal ]; then
     cd /opt/baikal
-    sudo -u www-data 'git pull'
-    sudo -u www-data 'composer install'
+    sudo -u www-data git pull
+    sudo -u www-data composer install
     sudo systemctl restart nginx
 
     exit 0
@@ -18,9 +18,9 @@ sudo mkdir baikal
 sudo chown -R www-data:www-data baikal
 sudo apt-get install -y php-fpm php-sqlite3 composer php-xml php-curl
 
-sudo -u www-data 'git clone https://github.com/sabre-io/Baikal baikal'
+sudo -u www-data git clone https://github.com/sabre-io/Baikal baikal
 cd baikal
-sudo -u www-data 'composer install'
+sudo -u www-data composer install
 
 # Adapted from https://sabre.io/baikal/install/
 # See also https://serverfault.com/a/870709/569995
