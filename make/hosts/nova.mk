3a0d8e4cebf662d61f148563585b4206d361a564
Olivia <zoe.i2k1@gmail.com>
2024-02-15 00:05:33 +0100

    le sigh

--- make/hosts/nova.mk
+++ make/hosts/nova.mk
@@ -7,7 +7,4 @@ nova-vpn:
 nova-baikal:
 	./automation/homelab/baikal.sh
 
-nova-huginn:
-	./automation/homelab/huginn.sh
-
-SOFTWARE_TARGETS += nova-uptime-kuma nova-vpn nova-baikal nova.huginn
+SOFTWARE_TARGETS += nova-uptime-kuma nova-vpn nova-baikal
