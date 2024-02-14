718669dada9b06b66cf169f44ebd50455b1a8af9
Olivia <zoe.i2k1@gmail.com>
2024-02-15 00:22:18 +0100

    Huginn is apparently very specific about its headers

--- automation/homelab/huginn.sh
+++ automation/homelab/huginn.sh
@@ -38,11 +38,14 @@ server {
     location / {
         proxy_set_header   X-Real-IP $remote_addr;
         proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header  X-Forwarded-Proto $scheme;
+        proxy_set_header  X-Forwarded-Host $host;
         proxy_set_header   Host $host;
         proxy_pass         http://localhost:6905/;
         proxy_http_version 1.1;
         proxy_set_header   Upgrade $http_upgrade;
         proxy_set_header   Connection "upgrade";
+
     }
 }
 EOF
