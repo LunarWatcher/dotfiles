3e3d92c1ef9ed5e694563c49686318fa81c1db7f
Olivia <zoe.i2k1@gmail.com>
2024-04-01 17:00:55 +0200

    Autolink .emacs

--- make/distros/mint.mk
+++ make/distros/mint.mk
@@ -9,20 +9,27 @@ mint-tweaks:
 	sudo gem install lolcat
 
 mint-home-packages:
+	# Flatpak libreoffice is much more up to date 
 	sudo apt purge libreoffice* && sudo apt autoremove
 	flatpak install flathub org.libreoffice.LibreOffice
 	flatpak install -y flathub com.uploadedlobster.peek
 	flatpak install -y flathub com.bitwarden.desktop
 	flatpak install -y flathub org.kde.krita
+	
+	# What have I done?
+	sudo apt install -y emacs
 
 mint-core:
 	sudo add-apt-repository ppa:papirus/papirus
 	sudo apt update && sudo apt install papirus-icon-theme
 	sudo apt install texlive-full
+	
+mint-home-dotfiles:
+	ln -sf ${PWD}/.emacs ${HOME}/.emacs
 
 mint-debloat:
 	sudo apt remove -y hexchat hypnotix transmission-gtk simple-scan
 
-HOME_TARGETS += mint-home-packages
+HOME_TARGETS += mint-home-packages mint-home-dotfiles
 SOFTWARE_TARGETS += mint-tweaks mint-core mint-tweaks
 CLEANUP_TARGETS += mint-debloat
