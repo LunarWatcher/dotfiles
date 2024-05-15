5d5e502ad09800cf60d3318271e7410908e46a0d
Olivia <zoe.i2k1@gmail.com>
2024-05-15 15:56:15 +0200

    Correct a series of dumb mistakes

--- opt/cron-scripts/qbittorrent-portforward.sh
+++ opt/cron-scripts/qbittorrent-portforward.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-$base_addr=${QBT_ADDR:-nova.lan}
-$torrent_addr="$base_addr:8080"
+base_addr=${QBT_ADDR:-nova.lan}
+torrent_addr="$base_addr:8080"
 
-port=$(curl http://$base_addr:8000/v1/openvpn/portforwarded | jq .port)
+port=$(curl --silent http://$base_addr:8000/v1/openvpn/portforwarded | jq .port)
 if [[ "$?" != "0" ]]; then
     ntfy pub --tags fire -p 5 --title="Cron failure [Gluetun]" alerts "Gluetun appears to be dead"
     exit -1
@@ -13,10 +13,10 @@ echo "Found port $port. Refreshing qbittorrent..."
 
 curl --fail --silent --show-error \
     --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt \
-    --header "Referer: $torrent_addr" --data "username=$QBT_USERNAME" --data "password=$QBT_PASSWORD" \
-    $torrent_addr/api/v2/auth/login 1> /dev/null
+    --header "Referer: http://$torrent_addr" --data "username=$QBT_USERNAME" --data "password=$QBT_PASSWORD" \
+    http://$torrent_addr/api/v2/auth/login 1> /dev/null
 
-listen_port=$(curl --fail --silent --show-error --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt $torrent_addr/v2/app/preferences | jq .listen_port)
+listen_port=$(curl --fail --silent --show-error -v --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt http://$torrent_addr/api/v2/app/preferences | jq .listen_port)
 
 if [ ! "$listen_port" ]; then
     ntfy pub --tags fire -p 5 --title "Cron failure [Gluetun]" alerts "Failed to get port from qbittorrent"
@@ -31,4 +31,6 @@ fi
 echo "Updating port to $port"
 
 curl --fail --silent --show-error --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt  \
-    --data-urlencode "json={\"listen_port\": $port}"  $torrent_addr/api/v2/app/setPreferences
+    --data-urlencode "json={\"listen_port\": $port}"  http://$torrent_addr/api/v2/app/setPreferences
+
+rm /tmp/cookies.txt
