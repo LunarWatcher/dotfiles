fbde7d07597d8f759b7e7801f89d25f7d4827a06
Olivia <zoe.i2k1@gmail.com>
2024-10-27 21:04:59 +0100

    More escaping

--- make/distros/mint.mk
+++ make/distros/mint.mk
@@ -44,7 +44,7 @@ mint-home-dotfiles:
 docker:
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$$UBUNTU_CODENAME") stable" | \
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $$(. /etc/os-release && echo "$$UBUNTU_CODENAME") stable" | \
 		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	
 	sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
