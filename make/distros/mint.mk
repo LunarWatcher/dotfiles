27d1c90f6ac7cc44609e86eb4bc39406f32a305f
Olivia <zoe.i2k1@gmail.com>
2024-10-27 20:15:42 +0100

    Forgot saving

--- make/distros/mint.mk
+++ make/distros/mint.mk
@@ -27,6 +27,12 @@ mint-home-packages:
 	sudo dpkg -i discord.deb
 	rm discord.deb
 
+	sudo mkdir -p /etc/apt/keyrings
+	sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+	sudo apt-get update
+	sudo apt-get install syncthing
+
 mint-core:
 	sudo add-apt-repository ppa:papirus/papirus
 	sudo apt update && sudo apt install papirus-icon-theme
