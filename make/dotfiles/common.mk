0188ffab4d285ed2b50c24e6ab5843ace559261c
Olivia <oliviawolfie@pm.me>
2026-05-30 19:38:35 +0200

    Remove rsync
    Fortunately not load-bearing software in my config, but it does fuck up
    my backups.
    Rsync now being slop also takes out timeshift, so my backups are now
    gone. Got pushed to 3.4.3 by arch, so uninstalled timeshift before rsync
    has a chance to fuck up my system.
    
    Not really sure what to do about backups. Rsync has been so stable for
    so long  that a lot of infrastructure relies on it, and a lot of backup
    software is advanced rsync use in a trenchcoat.
    
    Based on the arch wiki:
        https://wiki.archlinux.org/title/Synchronization_and_backup_programs
    As well as a few spot checks, the list of non-rsync-based backup
    software meant for entire machines locally that also doesn't reek of AI
    slop and is actively maintained is extremely short.
    
    Third party aggregate issue for the rsync clusterfuck:
        https://github.com/void-linux/void-packages/issues/60825
    
    Fuck AI slop

--- make/dotfiles/common.mk
+++ make/dotfiles/common.mk
@@ -3,24 +3,26 @@ emacs:
 
 	mkdir ${HOME}/.emacs-saves
 
-zellij:
-	mkdir -p ~/.config/zellij
-	ln -sf ${PWD}/config/zellij/config.kdl ${HOME}/.config/zellij/config.kdl
-
-rofi:
-	mkdir -p ${HOME}/.config/rofi
-	mkdir -p ${HOME}/.config/rofi/themes
-
-	ln -sf ${PWD}/config/rofi/config.rasi ${HOME}/.config/rofi/config.rasi
-	ln -sf ${PWD}/config/rofi/themes/catppuccin-latte.rasi ${HOME}/.config/rofi/themes/catppuccin-latte.rasi
-
 zsh-deps:
 	-git clone https://codeberg.org/LunarWatcher/amethyst ~/.config/amethyst
 
 config:
-	rsync -av --progress config/ ~/.config/
+	# TODO: find replacement for rsync: https://github.com/RsyncProject/rsync/issues/929
+	# rsync -av --progress config/ ~/.config/
 
-common-dotfiles: config emacs rofi zellij zsh-deps
+	mkdir -p ~/.config
+	mkdir -p ~/.config/rofi
+	mkdir -p ~/.config/rofi/themes
+	mkdir -p ~/.config/zellij
+
+	cp config/kcalcrc ~/.config
+
+	ln -sf ${PWD}/config/rofi/config.rasi ${HOME}/.config/rofi/config.rasi
+	ln -sf ${PWD}/config/rofi/themes/catppuccin-latte.rasi ${HOME}/.config/rofi/themes/catppuccin-latte.rasi
+
+	ln -sf ${PWD}/config/zellij/config.kdl ${HOME}/.config/zellij/config.kdl
+
+common-dotfiles: config emacs zsh-deps
 	ln -sf ${PWD}/.zshrc ${HOME}/.zshrc
 	ln -sf ${PWD}/.shell_aliases ${HOME}/.shell_aliases
 	ln -sf ${PWD}/.p10k.zsh ${HOME}/.p10k.zsh
